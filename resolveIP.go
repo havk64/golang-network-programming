0ba8efa9b99677364d0cdb2774b127e423f460dd
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-02-13 03:06:29 -0200

    Add basic IP resolver

---
+++ resolveIP.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <hostname>\n", os.Args[0])
+		fmt.Println("Usage: ", os.Args[0], "hostname")
+		os.Exit(1)
+	}
+	name := os.Args[1]
+
+	addr, err := net.ResolveIPAddr("ip", name)
+	if err != nil {
+		fmt.Println("Resolution error", err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("Resolved address is: %v", addr.String())
+	os.Exit(0)
+}
