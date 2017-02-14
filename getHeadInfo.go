220cbce970367a046fcd6dd3630819dbaabb0682
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-02-13 22:59:24 -0200

    Error handling

--- getHeadInfo.go
+++ getHeadInfo.go
@@ -15,26 +15,26 @@ func main() {
 	service := os.Args[1]
 
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
-	checkError(err)
+	checkError("Resolve error", err)
 
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	checkError(err)
+	checkError("Socket error", err)
+	fmt.Printf("conn: %#v\n", conn)
 
 	_, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
-	checkError(err)
+	checkError("Write error", err)
 
-	//result, err := readFully(conn)
 	result, err := ioutil.ReadAll(conn)
-	checkError(err)
+	checkError("Read error", err)
 
 	fmt.Println(string(result))
 
 	os.Exit(0)
 }
 
-func checkError(err error) {
+func checkError(s string, err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		fmt.Fprintf(os.Stderr, "%s: %s", s, err.Error())
 		os.Exit(1)
 	}
 }
