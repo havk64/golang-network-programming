f71dc3d40bee96f8354aee7294b81fb008598a60
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-02-14 15:45:30 -0200

    Add explanation to source code

--- dayTimeClient.go
+++ dayTimeClient.go
@@ -15,29 +15,30 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Usage: %s <host:port>\n", os.Args[0])
 		os.Exit(1)
 	}
-
+	// Convert first argument(Args[1]) to binary address and fill a TCPAddr struct
 	addr, err := net.ResolveTCPAddr("tcp", os.Args[1])
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Address error, %s\n", err.Error())
 		os.Exit(1)
 	}
-
+	// Create a TCP Socket and connect to address:port from the TCPAddr struct
+	// and returns a file descriptor
 	conn, err := net.DialTCP("tcp", nil, addr)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Socket error %s\n", err.Error())
 		os.Exit(1)
 	}
-
+	// Create a little buffer to read the http response
 	buf := make([]byte, 64)
-	for {
+	for { // The conn file descriptor is an 'io.Reader' so it implements the Read method
 		n, err := conn.Read(buf[0:])
 		if err != nil {
-			if err.Error() == "EOF" {
+			if err.Error() == "EOF" { // Exit loop when read is done
 				break
-			}
+			} // Otherwise report the read error and exit
 			fmt.Fprintf(os.Stderr, "Read error: %s\n", err.Error())
 			os.Exit(1)
-		}
+		} // Prints the response(type []byte) as string
 		fmt.Printf("%s", string(buf[0:n]))
 	}
 	os.Exit(0)
