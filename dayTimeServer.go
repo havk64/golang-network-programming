ff53c9a0f3483202ef116279567a0a36e95c8c95
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-02-14 00:01:02 -0200

    Add basic Daytime Server

---
+++ dayTimeServer.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+
+	service := ":1200"
+	tcpAddr, err := net.ResolveTCPAddr("tcp", service)
+	checkError(err)
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+	fmt.Printf("Listening on localhost:%d...\n", tcpAddr.Port)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+
+		daytime := time.Now().String()
+		conn.Write([]byte(daytime))
+		conn.Close()
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
