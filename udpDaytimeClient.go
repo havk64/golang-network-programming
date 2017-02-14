fee0f4bd846b6d235e3df3230cf946bf54b08fdf
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-02-14 20:55:19 -0200

    Add top level comments and code explanations

--- udpDaytimeClient.go
+++ udpDaytimeClient.go
@@ -1,3 +1,7 @@
+// UDP Daytime client
+//
+// Usage example:
+//   $ go run udpDaytimeClient.go localhost:1200
 package main
 
 import (
@@ -12,20 +16,24 @@ func main() {
 		os.Exit(1)
 	}
 	service := os.Args[1]
-
+	// Convert first argument(Args[1]) to binary address and fill a UDPAddr struct
 	udpAddr, err := net.ResolveUDPAddr("udp4", service)
 	checkError("Address", err)
-
+	// Optionally prints the IP address:(uncomment the line below)
+	// fmt.Printf("IP: %s\n", udpaddr.IP)
+	//
+	// Create a UDP Socket and connect to address:port from the UDPAddr struct
+	// and returns a file descriptor
 	conn, err := net.DialUDP("udp", nil, udpAddr)
 	checkError("Connection", err)
 
 	_, err = conn.Write([]byte("anything"))
 	checkError("Write", err)
-
+	// Create a buffer to read the response
 	var buf [512]byte
-	n, err := conn.Read(buf[0:])
+	n, err := conn.Read(buf[0:]) // Read the response
 	checkError("Read", err)
-
+	// Prints the response(type []byte) as string
 	fmt.Println(string(buf[0:n]))
 
 	os.Exit(0)
