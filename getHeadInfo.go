e85a12636a633bd08241c954220846e812973f4a
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-02-13 23:35:23 -0200

    Add comments

--- getHeadInfo.go
+++ getHeadInfo.go
@@ -1,3 +1,6 @@
+// Make a http HEAD request to an address:port specified in the command line
+// Example:
+//   $ go run getHeadInfo.go www.golang.org:80
 package main
 
 import (
@@ -13,18 +16,18 @@ func main() {
 		os.Exit(1)
 	}
 	service := os.Args[1]
-
+	// Resolve IP and return a TCPAddr struct with IP address and Port
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
 	checkError("Resolve error", err)
-
+	// Create the socket, connect and return its file descriptor
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	checkError("Socket error", err)
-
+	// Send the HEAD request
 	_, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n" +
 		"User-Agent: Golang Tcp client\r\n" +
 		"\r\n"))
 	checkError("Write error", err)
-
+	// Read the response
 	result, err := ioutil.ReadAll(conn)
 	checkError("Read error", err)
 
