e36083f966056f6f832ed266b94cf87e8ef2f13d
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-02-18 20:54:19 -0200

    Add load from gob file

---
+++ socket-level_programming/serialization/Gob/loadFromGob.go
@@ -0,0 +1,42 @@
+// Needs to be compiled with the file person.go
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var person Person
+	loadGob("person.gob", &person)
+	fmt.Printf("%s\n", person.String())
+}
+
+func loadGob(fileName string, key interface{}) {
+
+	inFile, err := os.Open(fileName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Opening file: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	decoder := gob.NewDecoder(inFile)
+	err = decoder.Decode(key)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Encode: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	inFile.Close()
+}
+
+// Implement fmt.Stringer interface
+func (p Person) String() string {
+	s := p.Name.Personal + " " + p.Name.Family
+	for _, v := range p.Email {
+		s += "\n" + v.Kind + ": " + v.Address
+	}
+	return s
+}
