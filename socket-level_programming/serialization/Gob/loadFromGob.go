8d8a64f2bb94f93b0fe1ac3a3b8cdb95a87f132b
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-02-18 21:14:37 -0200

    Move stringer to struct file

--- socket-level_programming/serialization/Gob/loadFromGob.go
+++ socket-level_programming/serialization/Gob/loadFromGob.go
@@ -31,12 +31,3 @@ func loadGob(fileName string, key interface{}) {
 
 	inFile.Close()
 }
-
-// Implement fmt.Stringer interface
-func (p Person) String() string {
-	s := p.Name.Personal + " " + p.Name.Family
-	for _, v := range p.Email {
-		s += "\n" + v.Kind + ": " + v.Address
-	}
-	return s
-}
