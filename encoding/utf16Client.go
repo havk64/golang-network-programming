75721ef7acd10b6fa041927187dd7bed594097a8
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-02-21 03:50:49 -0300

    Add utf16 client

---
+++ encoding/utf16Client.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"unicode/utf16"
+)
+
+const BOM = '\ufffe'
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ", os.Args[0], "host:port")
+		os.Exit(1)
+	}
+	service := os.Args[1]
+
+	conn, err := net.Dial("tcp", service)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Resolve: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	shorts := readShorts(conn)
+	ints := utf16.Decode(shorts)
+	str := string(ints)
+
+	fmt.Println(str)
+
+	os.Exit(0)
+}
+
+func readShorts(conn net.Conn) []uint16 {
+	var buf [512]byte
+
+	// read everything into the buffer
+	n, err := conn.Read(buf[0:2])
+	for true {
+		m, err := conn.Read(buf[n:])
+		if m == 0 || err != nil {
+			break
+		}
+		n += m
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Read: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	var shorts []uint16
+	shorts = make([]uint16, n/2)
+
+	if buf[0] == 0xff && buf[1] == 0xfe {
+		// big endian
+		for i := 2; i < n; i += 2 {
+			shorts[i/2] = uint16(buf[i])<<8 + uint16(buf[i+1])
+		}
+	} else if buf[1] == 0xff && buf[0] == 0xfe {
+		// little endian
+		for i := 2; i < n; i += 2 {
+			shorts[i/2] = uint16(buf[i+1])<<8 + uint16(buf[i])
+		}
+	} else {
+		// unknown byte order
+		fmt.Println("Unknown order")
+	}
+	return shorts
+
+}
