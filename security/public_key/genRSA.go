368a5ec47686672c0e914e4fccc107e7c23be39f
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-02-27 02:28:08 -0300

    Implement a stringer to the rsa object

--- security/public_key/genRSA.go
+++ security/public_key/genRSA.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
@@ -10,12 +11,19 @@ import (
 	"os"
 )
 
+type rsaStringer struct {
+	*rsa.PrivateKey
+}
+
 func main() {
 	// Random number generator posteriorly used to generate the key
 	reader := rand.Reader
 	bitSize := 512 // key size
 	key, err := rsa.GenerateKey(reader, bitSize)
 	checkError(err)
+
+	r := rsaStringer{key}
+	fmt.Printf("%s\n", r)
 
 	publicKey := key.PublicKey
 
@@ -46,6 +54,31 @@ func savePEMKey(fileName string, key *rsa.PrivateKey) {
 	pem.Encode(outFile, privateKey)
 
 	outFile.Close()
+}
+
+func (r rsaStringer) String() string {
+	var stream bytes.Buffer
+	fmt.Fprintf(&stream, "rsa.PrivateKey{\n")
+	fmt.Fprintf(&stream, "    PublicKey:\n")
+	fmt.Fprintf(&stream, "        rsa.PublicKey:{\n")
+	fmt.Fprintf(&stream, "            N: %v,\n", r.PublicKey.N)
+	fmt.Fprintf(&stream, "            E: %v\n", r.PublicKey.E)
+	fmt.Fprintf(&stream, "        },\n")
+	fmt.Fprintf(&stream, "    D: %v,\n", r.D)
+	fmt.Fprintf(&stream, "    Primes: []*big.Int[{\n")
+	fmt.Fprintf(&stream, "       %v,\n", r.Primes[0])
+	fmt.Fprintf(&stream, "       %v\n", r.Primes[1])
+	fmt.Fprintf(&stream, "    }],\n")
+	fmt.Fprintf(&stream, "    Precomputed:\n")
+	fmt.Fprintf(&stream, "        rsa.PrecomputedValues{\n")
+	fmt.Fprintf(&stream, "            Dp: %v,\n", r.Precomputed.Dp)
+	fmt.Fprintf(&stream, "            Dq: %v,\n", r.Precomputed.Dq)
+	fmt.Fprintf(&stream, "            Qinv: %v,\n", r.Precomputed.Qinv)
+	fmt.Fprintf(&stream, "            CRTValues: %#v\n", r.Precomputed.CRTValues)
+	fmt.Fprintf(&stream, "        }\n")
+	fmt.Fprintf(&stream, "}\n")
+
+	return stream.String()
 }
 
 func checkError(err error) {
